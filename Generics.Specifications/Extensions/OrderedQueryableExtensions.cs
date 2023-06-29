07c94caea05f53546d75d9fe218cfe07a41936a9
Theo <taharkes@gmail.com>
2023-06-29 23:12:29 +0200

    Moved: ThenBy extension method to QueryableExtensions

--- Generics.Specifications/Extensions/OrderedQueryableExtensions.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Linq.Expressions;
-
-namespace Generics.Specifications.Extensions {
-    public static class OrderedQueryableExtensions {
-        public static IOrderedQueryable<TSource> ThenBy<TSource, TKey>(this IOrderedQueryable<TSource> source, Expression<Func<TSource, TKey>> keySelector, bool descending)
-            => descending ? source.ThenByDescending(keySelector) : source.ThenBy(keySelector);
-    }
-}
