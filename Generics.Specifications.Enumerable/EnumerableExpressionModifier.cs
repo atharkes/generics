97ec48e14ae251c8dac34637b049321ef574bdee
Theo Harkes <t.harkes@betabit.nl>
2023-10-10 09:46:55 +0200

    Added: existing version of expression modifier; Todo: test & fix orderby in includes

---
+++ Generics.Specifications.Enumerable/EnumerableExpressionModifier.cs
@@ -0,0 +1,15 @@
+﻿using Generics.Specifications.Extensions;
+using System.Linq.Expressions;
+
+namespace Generics.Specifications.Enumerable {
+    public class EnumerableExpressionModifier : ExpressionVisitor {
+        public static readonly EnumerableExpressionModifier Default = new();
+
+        protected override Expression VisitMethodCall(MethodCallExpression node)
+            => QueryableIncludeExtensions.IsIncludeMethod(node.Method)
+            || QueryableIncludeExtensions.IsThenIncludeAfterEnumerableMethod(node.Method)
+            || QueryableIncludeExtensions.IsThenIncludeAfterReferenceMethod(node.Method)
+            ? Visit(node.Arguments[0])
+            : base.VisitMethodCall(node);
+    }
+}
