de509a43191933b0ce8a34742728354af696a563
Theo Harkes <t.harkes@betabit.nl>
2023-10-17 15:20:53 +0200

    Started adding include methods for enumerables

--- Generics.Specifications/Extensions/QueryableIncludeExtensions.cs
+++ Generics.Specifications/Extensions/QueryableIncludeExtensions.cs
@@ -5,6 +5,9 @@ using System.Reflection;
 namespace Generics.Specifications.Extensions {
     public interface IIncludableQueryable<out T, out TProperty> : IQueryable<T> { }
 
+
+    public interface IIncludableEnumerable<out T, out TProperty> : IEnumerable<T> { }
+
     public static class QueryableIncludeExtensions {
         internal static readonly MethodInfo IncludeMethodInfo = typeof(QueryableIncludeExtensions).GetTypeInfo().GetDeclaredMethods(nameof(Include)).Single(IsIncludeMethod);
         internal static readonly MethodInfo ThenIncludeAfterEnumerableMethodInfo = typeof(QueryableIncludeExtensions).GetTypeInfo().GetDeclaredMethods(nameof(ThenInclude)).Single(IsThenIncludeAfterEnumerableMethod);
@@ -33,6 +36,11 @@ namespace Generics.Specifications.Extensions {
             return new IncludableQueryable<T, TProperty>(source.Provider.CreateQuery<T>(expression));
         }
 
+        public static IIncludableEnumerable<T, TProperty> Include<T, TProperty>(this IEnumerable<T> source, Func<T, TProperty> navigationExpression) {
+            _ = source.Select(navigationExpression.Invoke);
+            return new IncludableEnumerable<T, TProperty>(source);
+        }
+
         public static IIncludableQueryable<T, TProperty> ThenInclude<T, TPreviousProperty, TProperty>(this IIncludableQueryable<T, IEnumerable<TPreviousProperty>> source, Expression<Func<TPreviousProperty, TProperty>> navigationExpression) {
             var expression = Expression.Call(null, ThenIncludeAfterEnumerableMethodInfo.MakeGenericMethod(typeof(T), typeof(TPreviousProperty), typeof(TProperty)), new[] { source.Expression, Expression.Quote(navigationExpression) });
             return new IncludableQueryable<T, TProperty>(source.Provider.CreateQuery<T>(expression));
@@ -54,6 +62,16 @@ namespace Generics.Specifications.Extensions {
             public IQueryProvider Provider => _queryable.Provider;
             public IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default) => ((IAsyncEnumerable<T>)_queryable).GetAsyncEnumerator(cancellationToken);
             public IEnumerator<T> GetEnumerator() => _queryable.GetEnumerator();
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+        private sealed class IncludableEnumerable<T, TProperty> : IIncludableEnumerable<T, TProperty> {
+            private readonly IEnumerable<T> _enumerable;
+
+            public IncludableEnumerable(IEnumerable<T> enumerable)
+                => _enumerable = enumerable;
+
+            public IEnumerator<T> GetEnumerator() => _enumerable.GetEnumerator();
             IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
         }
     }
