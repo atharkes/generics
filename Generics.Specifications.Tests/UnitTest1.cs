9ae844e4e8cf46604b1375aee7fd0888ab6f6ed7
Theo <taharkes@gmail.com>
2023-10-17 20:09:24 +0200

    Added: test project

---
+++ Generics.Specifications.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+using AutoFixture.Xunit2;
+
+namespace Generics.Specifications.Tests {
+    public class UnitTest1 {
+        [Theory, InlineAutoData]
+        public void Test1() {
+
+        }
+    }
+}
