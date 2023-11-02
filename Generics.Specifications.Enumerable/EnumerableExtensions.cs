0d7dcc95bc32e40f321d885c98beff8e65467e67
Theo Harkes <t.harkes@betabit.nl>
2023-11-02 10:40:29 +0100

    Added some initial implementation for IEnuemrable.Modify

--- Generics.Specifications.Enumerable/EnumerableExtensions.cs
+++ Generics.Specifications.Enumerable/EnumerableExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Generics.Specifications.Interfaces;
+using System.Collections;
 
 namespace Generics.Specifications.Enumerable {
     public static class EnumerableExtensions {
@@ -7,5 +8,41 @@ namespace Generics.Specifications.Enumerable {
 
         public static IEnumerable<TResult> Apply<T, TResult>(this IEnumerable<T> enumerable, ISpecification<T, TResult> specification)
             => specification.Apply(enumerable.AsQueryable());
+
+        /// <summary>
+        /// References for further development:
+        /// https://www.csharp411.com/c-object-clone-wars/
+        /// https://devblogs.microsoft.com/premier-developer/dissecting-the-new-constraint-in-c-a-perfect-example-of-a-leaky-abstraction/
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TProperty"></typeparam>
+        /// <param name="enumerable"></param>
+        /// <param name="navigationFunction"></param>
+        /// <param name="modificationFunction"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> Modify<T, TProperty>(
+            this IEnumerable<T> enumerable,
+            Func<T, TProperty> navigationFunction,
+            Func<TProperty, TProperty> modificationFunction
+        ) => enumerable.Select(item => {
+            // Only works when parameterless constructor is defined
+            var newItem = Activator.CreateInstance<T>();
+
+            // Only works when both setters and getters are available
+            foreach (var property in item.GetType().GetProperties()) {
+                property.SetValue(newItem, property.GetValue(item));
+            }
+
+            var modifiedValue = modificationFunction(navigationFunction(item));
+            // TODO: Apply modified value to new item:
+            // navigationFunction(item) = modifiedValue;
+
+            return newItem;
+        });
+
+        private class ModifyEnumerableIterator<T> : IEnumerable<T> {
+            public IEnumerator<T> GetEnumerator() => throw new NotImplementedException();
+            IEnumerator IEnumerable.GetEnumerator() => throw new NotImplementedException();
+        }
     }
 }
--- Generics.Specifications.Tests/IncludeTests.cs
+++ Generics.Specifications.Tests/IncludeTests.cs
@@ -7,7 +7,7 @@ namespace Generics.Specifications.Tests {
     public class IncludeTests {
         public sealed class ClassA {
             public IEnumerable<ClassB> SubItems { get; set; }
-            public ClassB Item { get; set; }
+            public ClassB SubItem { get; set; }
         }
 
         public sealed class ClassB {
@@ -16,7 +16,7 @@ namespace Generics.Specifications.Tests {
         }
 
         public sealed class ClassC {
-            public uint value { get; set; }
+            public uint Value { get; set; }
         }
 
         [Theory, InlineAutoData]
@@ -30,7 +30,7 @@ namespace Generics.Specifications.Tests {
 
         [Theory, InlineAutoData]
         public void ThenIncludeAfterReferenceDoesntThrow(IEnumerable<ClassA> items) {
-            var specification = new QuerySpecification<ClassA>(q => q.Include(a => a.Item).ThenInclude(b => b.SubItems));
+            var specification = new QuerySpecification<ClassA>(q => q.Include(a => a.SubItem).ThenInclude(b => b.SubItems));
 
             var result = items.Apply(specification);
 
@@ -52,6 +52,22 @@ namespace Generics.Specifications.Tests {
             var specification = new QuerySpecification<ClassA>(q => q.Include(a => a.SubItems.OrderBy(b => b.Value)));
 
             var result = items.Apply(specification);
+
+            foreach (var item in result) {
+                item.SubItems.Should().BeInAscendingOrder(b => b.Value);
+            }
+        }
+
+        [Theory, InlineAutoData]
+        public void Modify_NoOperation_SameResult(IEnumerable<ClassA> items) {
+            var result = items.Modify(item => item.SubItems, subItems => subItems);
+
+            result.Should().BeEquivalentTo(items);
+        }
+
+        [Theory, InlineAutoData]
+        public void Modify_OrderBy_OrderedResult(IEnumerable<ClassA> items) {
+            var result = items.Modify(item => item.SubItems, subItems => subItems.OrderBy(i => i.Value));
 
             foreach (var item in result) {
                 item.SubItems.Should().BeInAscendingOrder(b => b.Value);
